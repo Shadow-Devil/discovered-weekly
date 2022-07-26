fab25a345cfdd3b63bf4cf6818fce36446e77e2d
fschn <fschnabel007@gmail.com>
2022-07-26 21:31:44 +0200

    add release radar

--- discovered_weekly.py
+++ discovered_weekly.py
@@ -2,6 +2,7 @@ import datetime as dt
 import logging
 import os
 import sys
+from typing import List
 
 from dotenv import dotenv_values
 import spotipy
@@ -18,6 +19,7 @@ logger.setLevel(logging.INFO)
 def main():
     # Override sample with non-sample file-based env variables,
     # and override both with actual env variables
+    logger.info(os.environ)
     config = {**dotenv_values("sample.env"), **dotenv_values(".env"), **os.environ}
     logger.info("Start discover weekly archiving")
     client = load_client(
@@ -48,7 +50,7 @@ def main():
     logger.info("Done discover weekly archiving")
 
 
-def load_client(client_id, client_secret, redirect_uri, username, refresh_token):
+def load_client(client_id: str, client_secret: str, redirect_uri: str, username: str, refresh_token: str) -> spotipy.Spotify:
     scopes = ["playlist-read-private", "playlist-modify-private"]
     # Authenticate
     auth_manager = SpotifyOAuth(
@@ -63,8 +65,7 @@ def load_client(client_id, client_secret, redirect_uri, username, refresh_token)
     return client
 
 
-def parse_this_week(client, discover_weekly_playlist_id):
-
+def parse_this_week(client: spotipy.Spotify, discover_weekly_playlist_id: str) -> (dt.date, List[str]):
     # Grab this week's Discover Weekly (DW) and parse for some info
     dw_items = client.playlist_items(discover_weekly_playlist_id)
     playlist_created = dt.datetime.strptime(
@@ -76,14 +77,34 @@ def parse_this_week(client, discover_weekly_playlist_id):
     return playlist_date, dw_uris
 
 
-def add_to_all_time_playlist(client, dw_uris, all_discovered_playlist_id):
+def add_to_all_time_playlist(client: spotipy.Spotify, dw_uris: List[str], all_discovered_playlist_id: str):
     # First, add to the all time DW
 
     # Determine total number of tracks
     total = client.playlist(all_discovered_playlist_id)["tracks"]["total"]
     # Now, query for the last 5 tracks
     offset = max(0, total - 5)
-    last_five = client.playlist_items(all_discovered_playlist_id, offset=offset)
+    last_five = client.playlist_items(all_discovered_playlist_id, offset=offset, limit=5)
+
+    # If the last 5 tracks match the last 5 from the current week, then we've already added
+    # this week's playlist.
+    match = len(last_five["items"]) >= 5 and all(
+        [
+            dw_uri == item["track"]["uri"]
+            for dw_uri, item in zip(dw_uris[-5:], last_five["items"])
+        ]
+    )
+    if match:
+        logger.info(
+            "This script has already been run for this week."
+            " Skipping add to all time playlist."
+        )
+        return
+
+
+    offset = max(0, total - 30 - 5)
+    last_five = client.playlist_items(all_discovered_playlist_id, offset=offset, limit=5)
+
     # If the last 5 tracks match the last 5 from the current week, then we've already added
     # this week's playlist.
     match = len(last_five["items"]) >= 5 and all(
@@ -102,7 +123,7 @@ def add_to_all_time_playlist(client, dw_uris, all_discovered_playlist_id):
     client.playlist_add_items(all_discovered_playlist_id, dw_uris)
 
 
-def add_to_weekly_archive(client, username, playlist_date, dw_uris):
+def add_to_weekly_archive(client: spotipy.Spotify, username: str, playlist_date, dw_uris):
     # Second, create the weekly archive playlist
     this_weeks_playlist = f"Discovered Week {playlist_date}"
 
